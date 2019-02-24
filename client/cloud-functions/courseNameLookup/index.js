23cc0200297553cab7d5a0cfa4116f8a5d5f3139
Tianyu Wang <terrywang@tianyu.ninja>
2019-02-24 14:22:01 -0500

    Cloud function update

--- client/cloud-functions/courseNameLookup/index.js
+++ client/cloud-functions/courseNameLookup/index.js
@@ -2,19 +2,18 @@
 const cloud = require('wx-server-sdk');
 const requestP = require('request-promise');
 
-const APIKEY = require('./APIKEY');
+const APIKEY = require('../APIKEY');
 
 cloud.init()
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  // const wxContext = cloud.getWXContext();
   var subject = event.searchTerm.replace(/[0-9]/g, '').replace(/\s/g, '');
   var course = event.searchTerm.replace(/^\D+/g, '').replace(/\s/g, '');
   var term = event.termCode;
   if (subject === '') subject = 'dummy';
   if (course === '') course = 'dummy';
-  const result = JSON.parse(await requestP(`https://tianyu.wang/courselookup/${term}/${subject}/${course}?key=${APIKEY}`));
+  const result = JSON.parse(await requestP(`https://tianyu.wang/api/courseNameLookup/${term}/${subject}/${course}?key=${APIKEY}`));
 
   return result;
 }
---
+++ client/cloud-functions/courseNumberLookup/index.js
@@ -0,0 +1,15 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk');
+const requestP = require('request-promise');
+
+const APIKEY = require('../APIKEY');
+
+cloud.init()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  var number = event.courseNumber;
+  const result = JSON.parse(await requestP(`https://tianyu.wang/api/courseNumberLookup/${number}?key=${APIKEY}`));
+
+  return result;
+}
